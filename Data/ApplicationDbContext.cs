be02610df235cc94fddc69305fccf7b921e81a5f
Ryan Roethle <ryanroethle7474@gmail.com>
2025-04-24 11:44:49 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrewerPredictorApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<ContentTypeEntity> ContentTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity relationships here
+            // For example:
+            // modelBuilder.Entity<YourEntity>()
+            //     .HasMany(e => e.RelatedEntities)
+            //     .WithOne(e => e.YourEntity)
+            //     .HasForeignKey(e => e.YourEntityId);
+        }
+    }
+}
