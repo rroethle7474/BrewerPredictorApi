ea0466b67ad3c92985f1802824ec43b776ea7aa3
Ryan Roethle <ryanroethle7474@gmail.com>
2025-04-24 21:05:34 -0500

    added data annotations for required and key columns.

--- Models/Entities/Message.cs
+++ Models/Entities/Message.cs
@@ -1,11 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace BrewerPredictorApi.Models.Entities
 {
     public class Message
     {
+        [Key] // Explicitly marks this as the primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Name { get; set; }
+        [Required]
         public string MessageText { get; set; }
         public bool HasResponded { get; set; }
         public DateTime CreatedOn { get; set; }
--- Models/Entities/Prediction.cs
+++ Models/Entities/Prediction.cs
@@ -1,12 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace BrewerPredictorApi.Models.Entities
 {
-    public class Prediction
+    public class Prediction 
     {
+        [Key] // Explicitly marks this as the primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string LastName { get; set; }
+        [Required]
         public int Wins { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
